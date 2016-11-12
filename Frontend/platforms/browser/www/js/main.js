a6a3dc7b4e4b7d53f430f76e3e3f971402efc2f8
Benji Pelletier <pelletierbenji@gmail.com>
2016-11-12 06:40:44 -0800

    Integrated Imgur API

--- Frontend/platforms/browser/www/js/main.js
+++ Frontend/platforms/browser/www/js/main.js
@@ -8,4 +8,29 @@ app.controller('mainCtrl', ['$scope', function($scope) {
 	  document.getElementById("submit").removeAttribute("disabled");
 	}
 
+	$scope.submit = function() {
+ 	   var FR = new FileReader();
+ 	   FR.onload = function(e) {
+ 	   	$scope.uploadImage(e.target.result);
+ 	   };       
+    FR.readAsDataURL( document.getElementById("file").files[0]);
+	}
+
+	$scope.uploadImage = function(img) {
+		var imgdata = img.replace(/.*,/, '');
+ 	     $.ajax({
+			url: "https://api.imgur.com/3/upload",
+			type: "POST",
+			datatype: "json",
+			data: {image: imgdata},
+			success: function(result) {
+              var id = result.data.id;
+              var url 'https://imgur.com/gallery/' + id;
+            },
+			beforeSend: function (xhr) {
+			    xhr.setRequestHeader("Authorization", "Client-ID 47b80fa9d95a5be");
+			}
+		});;
+	    
+	}
 }]);
